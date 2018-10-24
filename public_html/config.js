c79ae81970a5e7dbb8c3900ec92f4e05954206b9
Tom Swartz <tom.swartz07@gmail.com>
2018-10-24 08:49:24 -0400

    Fixup config settings

--- public_html/config.js
+++ public_html/config.js
@@ -11,8 +11,8 @@ Metric = false; // true or false
 
 // -- Map settings ----------------------------------------
 // The Latitude and Longitude in decimal format
-CONST_CENTERLAT = 45.0;
-CONST_CENTERLON = 9.0;
+CONST_CENTERLAT = 40.0;
+CONST_CENTERLON = -76.0;
 // The google maps zoom level, 0 - 16, lower is further out
 CONST_ZOOMLVL   = 5;
 
@@ -20,15 +20,15 @@ CONST_ZOOMLVL   = 5;
 // The default marker color
 MarkerColor	  = "rgb(127, 127, 127)";
 SelectedColor = "rgb(225, 225, 225)";
-StaleColor = "rgb(190, 190, 190)";
+StaleColor = "rgb(255, 100, 0)";
 
 // -- Site Settings ---------------------------------------
-SiteShow    = false; // true or false
+SiteShow    = true; // true or false
 // The Latitude and Longitude in decimal format
-SiteLat     = 45.0;
-SiteLon     = 9.0;
+SiteLat     = 40.10;
+SiteLon     = -76.28;
 
 SiteCircles = true; // true or false (Only shown if SiteShow is true)
 // In nautical miles or km (depending settings value 'Metric')
-SiteCirclesDistances = new Array(100,150,200);
+SiteCirclesDistances = new Array(10,50,100,150,200);
 
